f1db7342ad078ffe89c5cf9132407f2d0ee61b23
relocatebiz <relocatebiz@protonmail.com>
2022-03-10 13:16:31 +0100

    Add more content to explain on home

--- imports/ui/pages/Home/Home.jsx
+++ imports/ui/pages/Home/Home.jsx
@@ -35,6 +35,31 @@ export const Home = () => {
                     </li>
                 </ul>
             </div>
+            <div className="more-info">
+                <ul className="wrapped-content">
+                    <li>
+                        <h3>
+                            Have a request?
+                        </h3>
+                        <p>
+                            Keeping you business alive is a small thing to do in war times, but it will prove
+                            very valuable for your country and your people. If there is anything EU founders can help you with,
+                            feel free to create a new request or browse the list of things people have offered to help with.
+                        </p>
+                    </li>
+                    <li>
+                        <h3>
+                            Want to help?
+                        </h3>
+                        <p>
+                            If you have crisis management experience, contacts or some spare time and
+                            resources and want to participate in
+                            helping Ukrainian startups weather this difficult time, you can post about the different things you can offer.
+                            You can also browser a list of requests from UA founders and see if you can help out.
+                        </p>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
